3edfe0a2edad57aaa50cef02428ddfe841c48b77
Nicole260205 <nicolecoelho26@gmail.com>
2025-04-04 17:16:14 -0300

    deuMeuHorario

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,13 +1,27 @@
 import { useState } from "react";
 
-function searchBar(props){
-    return (
-      <div>
-        <h2>Search for in item</h2>
-        <form>
-            <label for="name-field">Name: </label>
-            <input id="name-field" type="text  "></input>
-        </form>
-      </div>
-    );
+function SearchBar(props) {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [type, setType] = useState("");
+  const [brand, setBrand] = useState("");
+
+
+  return (
+    <div>
+      <h2>Search for in item</h2>
+      <form>
+        <label for="name-field">Name: </label>
+        <input id="name-field" type="text" value={name} />
+        <label for="price-field">Max price: </label>
+        <input id="price-field" type="number" value={price} />
+        <label for="type-field">Type: </label>
+        <input id="type-field" type="text" value={type} />
+        <label for="brand-field">Brand: </label>
+        <input id="brand-field" type="text" value={brand} />
+        <button type="button">Search</button>
+      </form>
+    </div>
+  );
 }
+export default SearchBar
